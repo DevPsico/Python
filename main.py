c3147c02e63faa783faae4b76a78edbca08dea4c
DevPsico <ericsonsergio19844@gmail.com>
2025-12-10 10:58:44 -0300

    "Commit inicial do projeto Python com FastAPI"

---
+++ main.py
@@ -0,0 +1,12 @@
+class Produto:
+    """
+    Docstring for Produto
+    """
+
+    def __init__(self, nome : str, preco : float):
+        
+        self.nome : str = nome
+        self.preco : float = preco
+
+        
+        p1 = Produto()
