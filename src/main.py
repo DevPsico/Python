cfff23060c98acfe5e1fdb4b017ec828987cc7d9
DevPsico <ericsonsergio19844@gmail.com>
2025-12-15 11:55:47 -0300

    IMplementação paricla da main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
 from __future__ import annotations
+from typing import List
+from fastapi import FastAPI, HTTPException, status
+
+from src.models.produto_schemas import ProdutoCreate, ProdutoRead, ProdutoUpdate
+from src.services.produto_repo_memory import ProdutoRepoMemory
+from src.services.produto_service import ProdutoService
+
+app = FastAPI(
+    title="API de Gestão de Produtos",
+    version="1.0.0",
+    description="Uma API simples para gerenciar produtos usando FastAPI."
+    )
+
+# Instanciar o serviço de produtos
+repo = ProdutoRepoMemory()
+service = ProdutoService(repo)
+
+@app.post("/produtos/", response_model=ProdutoRead, status_code=status.HTTP_201_CREATED)
+def criar_produto(produto: ProdutoCreate) -> ProdutoRead:
+    novo_produto = service.criar_produto(
+        nome=produto.nome,
+        preco=produto.preco,
+        ativo=produto.ativo
+    )
+    return ProdutoRead.from_orm(novo_produto)
+
+@app.get("/produtos/{produto_id}", response_model=ProdutoRead, tags=["healthcheck"])
+def obter_produto(produto_id: int) -> ProdutoRead:
+    produto = service.obter_produto(produto_id)
+    if not produto:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
+    return ProdutoRead.from_orm(produto)
+
+@app.get("/produtos/", response_model=List[ProdutoRead])
+def listar_produtos() -> List[ProdutoRead]:
+    produtos = service.listar_produtos()
+    return [ProdutoRead.from_orm(produto) for produto in produtos]
